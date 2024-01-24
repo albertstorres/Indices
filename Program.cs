44161d24a1ac69a74f2c7b42c8ade442167cd112
Albert Sevy <albertstorres@gmail.com>
2024-01-23 22:11:17 -0300

    Finalizando o repositório da aula String/Indices

--- Program.cs
+++ Program.cs
@@ -5,7 +5,10 @@ namespace indices
     {
         static void Main(string[] args)
         {
-
+            var texto = "Texto de teste";
+            //MÉTODOS COM MESMA FUNCIONALIDADE DO JS.
+            Console.WriteLine(texto.IndexOf("f"));//RETORNA -1 (NÃO HOUVE OCORRÊNCIA)
+            Console.WriteLine(texto.LastIndexOf("e"));//RETORNA 13 (ÚLTIMA OCORRÊNCIA)
         }
     }
 }
